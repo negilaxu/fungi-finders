9726db58f8391430d5d13028ec77104c83308384
negilaxu <negi_laxu090@hotmail.com>
2026-04-08 00:36:06 +0530

    added little bit of transition

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -9,6 +9,11 @@ const currentFilters = {
     edible: "all",
 };
 
+cards.forEach((card, index) => {
+    const mushroomId = `mushroom-${index + 1}`;
+    card.style.viewTransitionName = `card-${mushroomId}`;
+});
+
 seasonalFilter.addEventListener("change", updateFilter);
 edibleFilter.addEventListener("change", updateFilter);
 
@@ -16,7 +21,12 @@ function updateFilter(e) {
     const filterType = e.target.name;
     currentFilters[filterType] = e.target.value;
 
-    filterCards();
+    if (!document.startViewTransition()) {
+        filterCards();
+        return;
+    }
+
+    document.startViewTransition(() => filterCards());
 }
 
 function filterCards() {
